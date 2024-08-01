b812d4900c024cc53e4c2639758cfd19ccf2f7c9
Sagi Smolarski <sagism@gmail.com>
2024-08-01 14:15:25 +0300

    basic price entry, installing of daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let price;
+	let quantity;
+	$: total = price * quantity;
+
+	function formatPrice(price) {
+		return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price);
+	}
+</script>
+
+<h1>Welcome to The Quotes Helper</h1>
+<input name="price" bind:value={price} placeholder="price" />
+<input name="quantity" bind:value={quantity} placeholder="quantity" />
+<div id="total">{formatPrice(total)}</div>
